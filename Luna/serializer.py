c0ce813c762cd0e8c21081fa6478d49aff785c2c
YuBin Moon <moon.pinnamon@gmail.com>
2019-08-05 08:42:30 +0900

    Update Python -> Electron ==================== HelloWorld!

--- Luna/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Hosts
-
-
-class HostsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hosts
-        fields = '__all__'
